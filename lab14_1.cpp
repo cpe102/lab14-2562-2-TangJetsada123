bbe279068464bbe7985ba79371323924c84926ff
lab314-17@labeng.local <lab314-17@labeng.local>
2020-02-25 17:03:47 +0700

    lab14 ver1

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,11 +1,26 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 
 int main(){
-	//Write your code here.
-	
-	
-	
+	int a =5;
+	char b = 'a';
+	char &c = b;
+	int *x; 
+	x = &a;
+	char *y;
+	y = &b;
+	int *z;
+	z =x;
+	cout << a <<" "<< b <<" "<< c <<" " << x <<" " <<(int*)y <<" " << z <<" "<<"\n";	
+	cout << &a<<" "<<(int*) &b<<" "<<(int*) &c <<" " << &x <<" " <<(int*)&y <<" " << z <<" "<<"\n";	
+	c = 'f';
+	cout << a <<" "<< b <<" "<< c <<" " <<(int*)x <<" " <<(int*)y <<" " << z <<" "<<"\n";
+	*y = 'W';
+	cout << a <<" "<< b <<" "<<c <<" " << x <<" " <<(int*)y <<" " << z <<" "<<"\n";
+	*x = 6;
+	cout << a <<" "<< b <<" "<<c <<" " << x <<" " <<(int*)y <<" " << z <<" "<<"\n";
+	*z = 7;
+	cout << a <<" "<< b <<" "<<c <<" " << x <<" " <<(int*) y <<" " << z <<" "<<"\n";
 	return 0;
 }
